bbf7e0c8dffb9e3ffc2d37fd23120423033af9bc
Luca Leon Happel <luca.happel@hhu.de>
2022-10-24 14:20:29 +0200

    added image collection

--- runtime_extra/lua/mysettings.lua
+++ runtime_extra/lua/mysettings.lua
@@ -3,6 +3,7 @@
 -- {{{ Image loading
 MyIMG = {}
 
+-- {{{ Image
 MyIMG.Image = {}
 
 function MyIMG.Image:new(o, url)
@@ -20,23 +21,25 @@ function MyIMG.Image:new(o, url)
   return o
 end
 
+function MyIMG.Image:command(seconds)
+  return 'ueberzug layer --parser bash 0< <(' ..
+            'declare -Ap add_command=( ' ..
+              '[action]="add" ' ..
+              '[identifier]="id0" ' ..
+              '[x]="' .. self.x .. '" ' ..
+              '[y]="' .. self.y .. '" ' ..
+              '[width]="' .. self.width .. '" ' ..
+              '[height]="' .. self.height .. '" ' ..
+              '[scaler]="' .. self.scaler .. '" ' ..
+              '[path]="' .. self.url .. '"); ' ..
+              'sleep ' .. tostring(seconds) .. ')'
+end
+
 function MyIMG.Image:show(seconds)
   seconds = seconds or 1
   if not self.shown then
     self.shown = true
-    self.pid = os.execute(
-      'ueberzug layer --parser bash 0< <(' ..
-        'declare -Ap add_command=( ' ..
-          '[action]="add" ' ..
-          '[identifier]="id0" ' ..
-          '[x]="' .. self.x .. '" ' ..
-          '[y]="' .. self.y .. '" ' ..
-          '[width]="' .. self.width .. '" ' ..
-          '[height]="' .. self.height .. '" ' ..
-          '[scaler]="' .. self.scaler .. '" ' ..
-          '[path]="' .. self.url .. '"); ' ..
-          'sleep 1)'
-    )
+    self.pid = os.execute(self:command(seconds))
     print("showing image" .. self.url)
   end
 end
@@ -48,6 +51,30 @@ function MyIMG.Image:hide()
     print("hiding image" .. self.url)
   end
 end
+-- }}}
+
+-- {{{ Collection 
+MyIMG.Collection = {}
+
+function MyIMG.Collection:new(o, namespace)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+  self.images = {}
+  self.namespace = vim.api.nvim_create_namespace(namespace)
+  return o
+end
+
+function MyIMG.Collection:add(image)
+  table.insert(self.images, image)
+end
+
+function MyIMG.Collection:show(seconds)
+  for _, image in ipairs(self.images) do
+    image:show(seconds)
+  end
+end
+-- }}}
 
 function MyIMG.showImg(url)
   local img = MyIMG.Image:new(nil, url)
@@ -62,4 +89,45 @@ function MyIMG.showFull(url)
   img.height = vim.api.nvim_win_get_height(0)
   img:show(5)
 end;
+
+function MyIMG.tsParseForImageNodes()
+  local query = vim.treesitter.query.parse_query('markdown_inline', '(image) @image')
+
+  local buffer = vim.api.nvim_get_current_buf()
+  local buf_highlighter = require('vim.treesitter.highlighter').active[buffer]
+
+  local line_count = vim.api.nvim_buf_line_count(buffer)
+  local ts_utils = require('nvim-treesitter.ts_utils')
+
+  local c = MyIMG.Collection:new(nil, "imagepreview")
+
+  buf_highlighter.tree:for_each_tree(function(tstree, tree)
+    if tree:lang() ~= 'markdown_inline' then
+      return
+    end
+
+    local root = tstree:root()
+    local iter = query:iter_captures(root, buffer, 0, line_count + 1)
+
+    for _, node, _ in iter do
+      local range = ts_utils.get_vim_range({ ts_utils.get_node_range(node) }, buffer)
+      local mark_id = vim.api.nvim_buf_set_extmark(
+        vim.fn.bufnr('%'),
+        vim.api.nvim_create_namespace('demo'),
+        range, -- range[1],
+        0, -- range[2],
+        {
+          virt_lines_above = true,
+          virt_lines = {
+            {{"PLACEHOLDER", "error"}},
+            {{"PLACEHOLDER", "error"}},
+            {{"PLACEHOLDER", "error"}},
+            {{"PLACEHOLDER", "error"}},
+            {{"PLACEHOLDER", "error"}},
+            {{"PLACEHOLDER", "error"}},
+        }
+      })
+    end
+  end)
+end
 -- }}}
