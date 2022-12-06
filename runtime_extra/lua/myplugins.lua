13bb79896c72f747e17e9cd550f47aa24cafac24
Luca Leon Happel <lucahappel99@gmx.de>
2022-12-06 13:52:06 +0100

    add color-preview to flutter

--- runtime_extra/lua/myplugins.lua
+++ runtime_extra/lua/myplugins.lua
@@ -136,6 +136,15 @@ require("flutter-tools").setup {
   widget_guides = {
     enabled = true,
   },
+  lsp = {
+    color = { -- show the derived colours for dart variables
+      enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+      background = true, -- highlight the background
+      foreground = false, -- highlight the foreground
+      virtual_text = true, -- show the highlight using virtual text
+      virtual_text_str = "■", -- the virtual text character to highlight
+    },
+  }
 }
 -- }}}
 
